6b63a4393520fd6150735c9e714555289c0d2599
shubinarti-glitch <291805091+shubinarti-glitch@users.noreply.github.com>
2026-06-08 23:40:32 +0700

    Add MainScreen scaffolding (delete Greeting)

--- app/src/main/java/com/example/gesturedemo/MainActivity.kt
+++ app/src/main/java/com/example/gesturedemo/MainActivity.kt
@@ -7,7 +7,6 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -20,10 +19,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             GestureDemoTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    MainScreen(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
@@ -31,17 +27,13 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MainScreen(modifier: Modifier = Modifier) {
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun MainScreenPreview() {
     GestureDemoTheme {
-        Greeting("Android")
+        MainScreen()
     }
 }
